fd344da8b41ae231c875f35c6de437b50c96ddb0
Ali Mohamed <ali.hgg@gmail.com>
2020-02-19 10:08:40 +0200

    fixed rating table relations

---
+++ Final Project Code First/Migrations/202002190758343_fix rating table.cs
@@ -0,0 +1,24 @@
+﻿namespace Final_Project_Code_First.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fixratingtable : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Ratings", name: "RateRatedUser_UserId", newName: "Rated_User_Id");
+            RenameColumn(table: "dbo.Ratings", name: "RateSenderUser_UserId", newName: "Sender_User_Id");
+            RenameIndex(table: "dbo.Ratings", name: "IX_RateSenderUser_UserId", newName: "IX_Sender_User_Id");
+            RenameIndex(table: "dbo.Ratings", name: "IX_RateRatedUser_UserId", newName: "IX_Rated_User_Id");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.Ratings", name: "IX_Rated_User_Id", newName: "IX_RateRatedUser_UserId");
+            RenameIndex(table: "dbo.Ratings", name: "IX_Sender_User_Id", newName: "IX_RateSenderUser_UserId");
+            RenameColumn(table: "dbo.Ratings", name: "Sender_User_Id", newName: "RateSenderUser_UserId");
+            RenameColumn(table: "dbo.Ratings", name: "Rated_User_Id", newName: "RateRatedUser_UserId");
+        }
+    }
+}
--- Final Project Code First/Models/Rating.cs
+++ Final Project Code First/Models/Rating.cs
@@ -11,19 +11,19 @@ namespace Final_Project_Code_First.Models
         [Key]
         public int Id { get; set; }
         ////[Key]
-        ////[ForeignKey("RateSenderUser")]
-        //[Column("Sender_User_Id",Order =0)]
-        //public int? SenderUserId { get; set; }
-
+        [Column("Sender_User_Id", Order = 0)]
+        public int? SenderUserId { get; set; }
+        [ForeignKey("SenderUserId")]
         public virtual User RateSenderUser { get; set; }
 
         [DataType("decimal(5,3)")]
         [Required]
         public decimal Rate { get; set; }
         //[Key]
-        //[ForeignKey("RateRatedUser")]
-        //[Column("Rated_User_Id",Order =1)]
-        //public int? RatedUserId { get; set; }
+        [Column("Rated_User_Id", Order = 1)]
+        public int? RatedUserId { get; set; }
+        [ForeignKey("RatedUserId")]
+
         public virtual User RateRatedUser { get; set; }
 
     }
