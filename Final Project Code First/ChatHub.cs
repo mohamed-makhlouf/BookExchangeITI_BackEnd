09d47874ae21f2b5698ea8b6647d4fedc6d963b8
iti <ayamohamed5017@yahoo.com>
2020-02-21 15:49:47 -0800

    chatsignalrv1

---
+++ Final Project Code First/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Final_Project_Code_First
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        
+        public void sendMessage( string name,string message)
+        {
+            //dbcode
+            Clients.All.newMessage(name , message);
+
+
+        }
+       
+    }
+}
