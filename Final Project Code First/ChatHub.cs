9069cb2a6c6d381f1d9b69c86493b2641c865cd5
iti <ayamohamed5017@yahoo.com>
2020-02-21 22:33:38 -0800

    added signalr

--- Final Project Code First/ChatHub.cs
+++ Final Project Code First/ChatHub.cs
@@ -10,17 +10,11 @@ namespace Final_Project_Code_First
     [HubName("chat")]
     public class ChatHub : Hub
     {
-        ChatEntities db = new ChatEntities();
         public void sendMessage( int senderId,string message)
         {
 
             //dbcode
             Clients.All.newMessage(senderId, message);
-            Chat mess = new Chat() { ChatSenderUser_UserId = senderId, Message = message, Date_Of_Message = DateTime.Now };
-
-            db.Chats.Add(mess);
-            db.SaveChanges();
-
         }
        
     }
